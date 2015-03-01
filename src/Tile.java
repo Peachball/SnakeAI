c659775e0d6c40bd16d91ef2c45706ad48e3841e
Benjamin <fellfromatree@gmail.com>
2015-03-01 12:18:33 -0800

    Update Tile.java

--- src/Tile.java
+++ src/Tile.java
@@ -1,6 +1,6 @@
 
 /**
- * This interface is the type that's stored in the array 
+ * This interface is the type that's stored in the array in Grid.java
  * @author Benjamin
  */
 public interface Tile {
